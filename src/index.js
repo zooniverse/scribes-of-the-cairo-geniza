1e6ee08696c9dd7071c6839368b5c75f177751ca
Amy Boyer <amy@zooniverse.org>
2016-12-07 15:31:52 -0600

    add back the stuff that i removed about initializing oauth client

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory, Router, Route } from 'react-router';
 import { Provider } from 'react-redux';
-// import oauth from 'panoptes-client/lib/oauth';
+import oauth from 'panoptes-client/lib/oauth';
 
 import App from './components/App';
 import PoweredBy from './components/Powered-by';
 import About from './components/About';
-// import { config } from './constants/config';
+import { config } from './constants/config';
 import configureStore from './store';
 
 // Todo: let's find a better way to include Styles,
@@ -18,14 +18,17 @@ const store = configureStore();
 
 window.React = React;
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <Route path="/about" component={About} />
-        <Route path="/poweredby" component={PoweredBy} />
-      </Route>
-    </Router>
-  </Provider>
-  , document.getElementById('root'),
-);
+oauth.init(config.panoptesAppId)
+  .then(() => {
+    ReactDOM.render((
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <Route path="/" component={App}>
+            <Route path="/about" component={About} />
+            <Route path="/poweredby" component={PoweredBy} />
+          </Route>
+        </Router>
+      </Provider>),
+      document.getElementById('root'),
+    );
+  });
