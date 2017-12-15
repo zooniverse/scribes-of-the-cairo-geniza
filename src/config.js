090ffb60bc0a71551e9422795cb5bbb8dc6f7920
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-12-15 19:28:59 +0000

    Add dynamic config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,51 @@
-const config = {
-  panoptesAppId: '24ad5676d5d25c6aa850dc5d5f63ec8c03dbc7ae113b6442b8571fce6c5b974c', // test-rog project for dev
+/*
+Configuration Settings
+----------------------
+
+The config settings change depending on which environment the app is running in.
+By default, this is the development environment, but this can be changed either by:
+
+- An ?env query string, e.g. localhost:3000?env=production
+- The NODE_ENV environment variable on the system running the app.
+
+ */
+
+var DEFAULT_ENV = 'development';
+var envFromBrowser = locationMatch(/\W?env=(\w+)/);
+var envFromShell = process.env.NODE_ENV;
+var env = envFromBrowser || envFromShell || DEFAULT_ENV;
+
+if (!env.match(/^(production|staging|development)$/)) {
+  throw new Error(`Error: Invalid Environment - ${env}`);
+}
+
+const baseConfig = {
+  development: {
+    panoptesAppId: '974cc8da2448bac692703f0b364a6b41a7662d91a5a3a1acb064eb703a01e6df', // Anti-Slavery Manuscripts on Staging
+    host: '',
+    projectId: '',
+    projectSlug: '',
+  },
+  production: {
+    panoptesAppId: '',
+    host: '',
+    projectId: '',
+    projectSlug: '',
+  }
 };
 
-export default config;
+baseConfig.staging = baseConfig.development;  //staging === development, as far as we're concerned.
+
+const config = baseConfig[env];
+
+export { env, config };
+
+// Try and match the location.search property against a regex. Basically mimics
+// the CoffeeScript existential operator, in case we're not in a browser.
+function locationMatch(regex) {
+  var match;
+  if (typeof location !== 'undefined' && location !== null) {
+    match = location.search.match(regex);
+  }
+  return (match && match[1]) ? match[1] : undefined;
+}
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import oauth from 'panoptes-client/lib/oauth';
 
 import App from './components/App';
-import config from './config';
+import { config } from './config';
 import configureStore from './store';
 
 import './styles/main.styl';
