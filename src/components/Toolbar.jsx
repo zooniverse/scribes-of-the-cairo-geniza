982ecf378de0387a3c8d75f68c10db98c4ffaa03
Will Granger <twgranger@gmail.com>
2018-02-02 16:16:33 -0600

    Add Expanded Toolbar Panel

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -21,6 +21,12 @@ class Toolbar extends React.Component {
     this.rotateSubject = this.rotateSubject.bind(this);
     this.resetView = this.resetView.bind(this);
     this.invertColors = this.invertColors.bind(this);
+    this.toggleIcon = this.toggleIcon.bind(this);
+    this.togglePanel = this.togglePanel.bind(this);
+
+    this.state = {
+      showPanel: false
+    };
   }
 
   useZoomIn() {
@@ -43,25 +49,78 @@ class Toolbar extends React.Component {
     this.props.dispatch(toggleContrast());
   }
 
-  render() {
+  toggleIcon() {
+    const iconClass = this.state.showPanel ? 'fa fa-chevron-left' : 'fa fa-chevron-right';
     return (
-      <section className="toolbar">
-        <button>&#x02A01;</button>
-        <button><i className="fa fa-arrows" /></button>
+      <button className="button-header" onClick={this.togglePanel}>
+        {this.state.showPanel && (
+          <span>Toolbar</span>
+        )}
+        <i className={`${iconClass} button-header`} />
+      </button>
+    );
+  }
+
+  togglePanel() {
+    this.setState({ showPanel: !this.state.showPanel });
+  }
+
+  render() {
+    const expanded = this.state.showPanel;
+    const toolbarClass = expanded ? 'toolbar toolbar__expanded' : 'toolbar';
+
+    return (
+      <section className={toolbarClass}>
+        {this.toggleIcon()}
 
         <hr />
 
-        <button onClick={this.useZoomIn}><i className="fa fa-plus" /></button>
-        <button onClick={this.useZoomOut}><i className="fa fa-minus" /></button>
-        <button onClick={this.rotateSubject}><i className="fa fa-repeat" /></button>
-        <button onClick={this.invertColors}><i className="fa fa-adjust" /></button>
-        <button><i className="fa fa-eye" /></button>
-        <button onClick={this.resetView}><i className="fa fa-refresh" /></button>
+        <button>
+          <i>&#x02A01;</i>
+          {expanded && (<span>Add Transcription</span>)}
+        </button>
+        <button>
+          <i className="fa fa-arrows" />
+          {expanded && (<span>Pan</span>)}
+        </button>
 
         <hr />
 
-        <button><i className="fa fa-heart-o" /></button>
-        <button><i className="fa fa-list" /></button>
+        <button onClick={this.useZoomIn}>
+          <i className="fa fa-plus" />
+          {expanded && (<span>Zoom In</span>)}
+        </button>
+        <button onClick={this.useZoomOut}>
+          <i className="fa fa-minus" />
+          {expanded && (<span>Zoom Out</span>)}
+        </button>
+        <button onClick={this.rotateSubject}>
+          <i className="fa fa-repeat" />
+          {expanded && (<span>Rotate</span>)}
+        </button>
+        <button onClick={this.invertColors}>
+          <i className="fa fa-adjust" />
+          {expanded && (<span>Invert Colors</span>)}
+        </button>
+        <button>
+          <i className="fa fa-eye" />
+          {expanded && (<span>Toggle Previous Marks</span>)}
+        </button>
+        <button onClick={this.resetView}>
+          <i className="fa fa-refresh" />
+          {expanded && (<span>Reset Image</span>)}
+        </button>
+
+        <hr />
+
+        <button>
+          <i className="fa fa-heart-o" />
+          {expanded && (<span>Add To Favorites</span>)}
+        </button>
+        <button>
+          <i className="fa fa-list" />
+          {expanded && (<span>Add To Collection</span>)}
+        </button>
       </section>
     );
   }
