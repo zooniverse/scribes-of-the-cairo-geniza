a5f11d533e69ea9221136435ce0c6ae64b7787c4
Will Granger <twgranger@gmail.com>
2018-03-29 17:13:34 -0500

    Add Missing Characters and Reorganize Letters

--- src/lib/ArabicKeyboard.js
+++ src/lib/ArabicKeyboard.js
@@ -1,41 +1,31 @@
 const MODERN_ARABIC = {
-  alef: {
-    character: '\u0627',
+  dad: {
+    character: '\u0636',
     characterID: 1,
-    name: 'alef',
+    name: 'dad',
     row: 0,
     unicode: {
-      initial: '\ufe8d',
-      isolated: '\u0627'
+      final: '\ufebe',
+      medial: '\ufec0',
+      initial: '\ufebf',
+      isolated: '\ufebd'
     }
   },
-  baa: {
-    character: '\u0628',
+  sad: {
+    character: '\u0635',
     characterID: 2,
-    name: 'baa',
+    name: 'sad',
     row: 0,
     unicode: {
-      final: '\ufe90',
-      medial: '\ufe92',
-      initial: '\ufe91',
-      isolated: '\ufe8f'
-    }
-  },
-  teh: {
-    character: '\u062a',
-    characterID: 3,
-    name: 'teh',
-    row: 0,
-    unicode: {
-      final: '\ufe96',
-      medial: '\ufe98',
-      initial: '\ufe97',
-      isolated: '\ufe95'
+      final: '\ufeba',
+      medial: '\ufebc',
+      initial: '\ufebb',
+      isolated: '\ufeb9'
     }
   },
   thaa: {
     character: '\u062B',
-    characterID: 4,
+    characterID: 3,
     name: 'thaa',
     row: 0,
     unicode: {
@@ -45,33 +35,69 @@ const MODERN_ARABIC = {
       isolated: '\ufe99'
     }
   },
-  jeem: {
-    character: '\u062c',
-    characterID: 5,
-    name: 'jeem',
+  qaf: {
+    character: '\u0642',
+    characterID: 4,
+    name: 'qaf',
     row: 0,
     unicode: {
-      final: '\ufe9e',
-      medial: '\uFEA0',
-      initial: '\ufe9f',
-      isolated: '\ufe9d'
+      final: '\ufed6',
+      medial: '\ufed8',
+      initial: '\ufed7',
+      isolated: '\ufed5'
     }
   },
-  hah: {
-    character: '\u062d',
-    characterID: 6,
-    name: 'hah',
+  feh: {
+    character: '\u0641',
+    characterID: 5,
+    name: 'feh',
     row: 0,
     unicode: {
-      final: '\ufea2',
-      medial: '\ufea4',
-      initial: '\ufea3',
-      isolated: '\ufea1'
+      final: '\ufed2',
+      medial: '\ufed4',
+      initial: '\ufed3',
+      isolated: '\ufed1'
+    }
+  },
+  ghain: {
+    character: '\u063a',
+    characterID: 6,
+    name: 'ghain',
+    row: 0,
+    unicode: {
+      final: '\ufece',
+      medial: '\ufed0',
+      initial: '\ufecf',
+      isolated: '\ufecd'
+    }
+  },
+  ain: {
+    character: '\u0639',
+    characterID: 7,
+    name: 'ain',
+    row: 0,
+    unicode: {
+      final: '\ufeca',
+      medial: '\ufecc',
+      initial: '\ufecb',
+      isolated: '\ufec9'
+    }
+  },
+  heh: {
+    character: '\u0647',
+    characterID: 8,
+    name: 'heh',
+    row: 0,
+    unicode: {
+      final: '\ufeea',
+      medial: '\ufeec',
+      initial: '\ufeeb',
+      isolated: '\ufee9'
     }
   },
   khah: {
     character: '\u062E',
-    characterID: 7,
+    characterID: 9,
     name: 'khah',
     row: 0,
     unicode: {
@@ -81,56 +107,38 @@ const MODERN_ARABIC = {
       isolated: '\ufea5'
     }
   },
-  dal: {
-    character: '\u062F',
-    characterID: 8,
-    name: 'dal',
+  hah: {
+    character: '\u062d',
+    characterID: 10,
+    name: 'hah',
     row: 0,
     unicode: {
-      final: '\ufeaa',
-      isolated: '\ufea9'
+      final: '\ufea2',
+      medial: '\ufea4',
+      initial: '\ufea3',
+      isolated: '\ufea1'
     }
   },
-  dahal: {
-    character: '\u068C',
-    characterID: 9,
-    name: 'dahal',
-    row: 1,
-    unicode: {
-      final: '\ufb85',
-      isolated: '\ufb84'
-    }
-  },
-  reh: {
-    character: '\u0631',
-    characterID: 10,
-    name: 'reh',
-    row: 1,
-    unicode: {
-      final: '\ufeae',
-      isolated: '\ufead'
-    }
-  },
-  zain: {
-    character: '\u0632',
+  jeem: {
+    character: '\u062c',
     characterID: 11,
-    name: 'zain',
-    row: 1,
+    name: 'jeem',
+    row: 0,
     unicode: {
-      final: '\ufeb0',
-      isolated: '\ufeaf'
+      final: '\ufe9e',
+      medial: '\uFEA0',
+      initial: '\ufe9f',
+      isolated: '\ufe9d'
     }
   },
-  seen: {
-    character: '\u0633',
+  tehMarbuta: {
+    character: '\u0629',
     characterID: 12,
-    name: 'seen',
-    row: 1,
+    name: 'heh',
+    row: 0,
     unicode: {
-      final: '\ufeb2',
-      medial: '\ufeb4',
-      initial: '\ufeb3',
-      isolated: '\ufeb1'
+      final: '\ufe94',
+      isolated: '\ufe93'
     }
   },
   sheen: {
@@ -145,119 +153,47 @@ const MODERN_ARABIC = {
       isolated: '\ufeb5'
     }
   },
-  sad: {
-    character: '\u0635',
+  seen: {
+    character: '\u0633',
     characterID: 14,
-    name: 'sad',
+    name: 'seen',
     row: 1,
     unicode: {
-      final: '\ufeba',
-      medial: '\ufebc',
-      initial: '\ufebb',
-      isolated: '\ufeb9'
+      final: '\ufeb2',
+      medial: '\ufeb4',
+      initial: '\ufeb3',
+      isolated: '\ufeb1'
     }
   },
-  dad: {
-    character: '\u0636',
+  yeh: {
+    character: '\u064a',
     characterID: 15,
-    name: 'dad',
+    name: 'yeh',
     row: 1,
     unicode: {
-      final: '\ufebe',
-      medial: '\ufec0',
-      initial: '\ufebf',
-      isolated: '\ufebd'
+      final: '\ufef2',
+      medial: '\ufef4',
+      initial: '\ufef3',
+      isolated: '\ufef1'
     }
   },
-  tah: {
-    character: '\u0637',
+  beh: {
+    character: '\u0628',
     characterID: 16,
-    name: 'tah',
+    name: 'beh',
     row: 1,
     unicode: {
-      final: '\ufec2',
-      medial: '\ufec4',
-      initial: '\ufec3',
-      isolated: '\ufec1'
-    }
-  },
-  zah: {
-    character: '\u0638',
-    characterID: 17,
-    name: 'zah',
-    row: 1,
-    unicode: {
-      final: '\ufec6',
-      medial: '\ufec8',
-      initial: '\ufec7',
-      isolated: '\ufec5'
-    }
-  },
-  ain: {
-    character: '\u0639',
-    characterID: 18,
-    name: 'ain',
-    row: 1,
-    unicode: {
-      final: '\ufeca',
-      medial: '\ufecc',
-      initial: '\ufecb',
-      isolated: '\ufec9'
-    }
-  },
-  ghain: {
-    character: '\u063a',
-    characterID: 19,
-    name: 'ghain',
-    row: 2,
-    unicode: {
-      final: '\ufece',
-      medial: '\ufed0',
-      initial: '\ufecf',
-      isolated: '\ufecd'
-    }
-  },
-  feh: {
-    character: '\u0641',
-    characterID: 20,
-    name: 'feh',
-    row: 2,
-    unicode: {
-      final: '\ufed2',
-      medial: '\ufed4',
-      initial: '\ufed3',
-      isolated: '\ufed1'
-    }
-  },
-  qaf: {
-    character: '\u0642',
-    characterID: 21,
-    name: 'qaf',
-    row: 2,
-    unicode: {
-      final: '\ufed6',
-      medial: '\ufed8',
-      initial: '\ufed7',
-      isolated: '\ufed5'
-    }
-  },
-  kaf: {
-    character: '\u0643',
-    characterID: 22,
-    name: 'kaf',
-    row: 2,
-    unicode: {
-      final: '\ufeda',
-      medial: '\ufedc',
-      initial: '\ufedb',
-      isolated: '\ufed9'
+      final: '\ufe90',
+      medial: '\ufe92',
+      initial: '\ufe91',
+      isolated: '\ufe8f'
     }
   },
   lam: {
     character: '\u0644',
-    characterID: 23,
+    characterID: 17,
     name: 'lam',
-    row: 2,
+    row: 1,
     unicode: {
       final: '\ufede',
       medial: '\ufee0',
@@ -265,23 +201,33 @@ const MODERN_ARABIC = {
       isolated: '\ufedd'
     }
   },
-  meem: {
-    character: '\u0645',
-    characterID: 24,
-    name: 'meem',
-    row: 2,
+  alef: {
+    character: '\u0627',
+    characterID: 18,
+    name: 'alef',
+    row: 1,
     unicode: {
-      final: '\ufee2',
-      medial: '\ufee4',
-      initial: '\ufee3',
-      isolated: '\ufee1'
+      initial: '\ufe8d',
+      isolated: '\u0627'
+    }
+  },
+  teh: {
+    character: '\u062a',
+    characterID: 19,
+    name: 'teh',
+    row: 0,
+    unicode: {
+      final: '\ufe96',
+      medial: '\ufe98',
+      initial: '\ufe97',
+      isolated: '\ufe95'
     }
   },
   nun: {
     character: '\u0646',
-    characterID: 25,
+    characterID: 20,
     name: 'nun',
-    row: 2,
+    row: 1,
     unicode: {
       final: '\ufee6',
       medial: '\ufee8',
@@ -289,21 +235,106 @@ const MODERN_ARABIC = {
       isolated: '\ufee5'
     }
   },
-  heh: {
-    character: '\u0647',
-    characterID: 26,
-    name: 'heh',
+  meem: {
+    character: '\u0645',
+    characterID: 21,
+    name: 'meem',
+    row: 1,
+    unicode: {
+      final: '\ufee2',
+      medial: '\ufee4',
+      initial: '\ufee3',
+      isolated: '\ufee1'
+    }
+  },
+  kaf: {
+    character: '\u0643',
+    characterID: 22,
+    name: 'kaf',
+    row: 1,
+    unicode: {
+      final: '\ufeda',
+      medial: '\ufedc',
+      initial: '\ufedb',
+      isolated: '\ufed9'
+    }
+  },
+  hamza: {
+    character: '\u0621',
+    characterID: 23,
+    name: 'hamza',
+    row: 1,
+    unicode: {
+      isolated: '\ufe80'
+    }
+  },
+  zah: {
+    character: '\u0638',
+    characterID: 24,
+    name: 'zah',
     row: 2,
     unicode: {
-      final: '\ufeea',
-      medial: '\ufeec',
-      initial: '\ufeeb',
-      isolated: '\ufee9'
+      final: '\ufec6',
+      medial: '\ufec8',
+      initial: '\ufec7',
+      isolated: '\ufec5'
+    }
+  },
+  tah: {
+    character: '\u0637',
+    characterID: 25,
+    name: 'tah',
+    row: 2,
+    unicode: {
+      final: '\ufec2',
+      medial: '\ufec4',
+      initial: '\ufec3',
+      isolated: '\ufec1'
+    }
+  },
+  dahal: {
+    character: '\u068C',
+    characterID: 26,
+    name: 'dahal',
+    row: 2,
+    unicode: {
+      final: '\ufb85',
+      isolated: '\ufb84'
+    }
+  },
+  dal: {
+    character: '\u062F',
+    characterID: 27,
+    name: 'dal',
+    row: 2,
+    unicode: {
+      final: '\ufeaa',
+      isolated: '\ufea9'
+    }
+  },
+  zain: {
+    character: '\u0632',
+    characterID: 28,
+    name: 'zain',
+    row: 2,
+    unicode: {
+      final: '\ufeb0',
+      isolated: '\ufeaf'
+    }
+  },
+  reh: {
+    character: '\u0631',
+    characterID: 29,
+    name: 'reh',
+    row: 2,
+    unicode: {
+      final: '\ufeae',
+      isolated: '\ufead'
     }
   },
   waw: {
     character: '\u0648',
-    characterID: 27,
+    characterID: 30,
     name: 'waw',
     row: 2,
     unicode: {
@@ -311,16 +342,32 @@ const MODERN_ARABIC = {
       isolated: '\ufeed'
     }
   },
-  yeh: {
-    character: '\u064a',
-    characterID: 27,
-    name: 'yeh',
+  alefLam: {
+    character: '\u0644',
+    characterID: 31,
+    name: 'alefLam',
     row: 2,
     unicode: {
-      final: '\ufef2',
-      medial: '\ufef4',
-      initial: '\ufef3',
-      isolated: '\ufef1'
+      final: '\ufefc',
+      isolated: '\ufefb'
+    }
+  },
+  behDotless: {
+    character: '\u066e',
+    characterID: 32,
+    name: 'behDotless',
+    row: 2,
+    unicode: {
+      isolated: '\u066e'
+    }
+  },
+  alefMaksura: {
+    character: '\u0649',
+    characterID: 33,
+    name: 'alefMaksura',
+    row: 2,
+    unicode: {
+      isolated: '\u0649'
     }
   }
 };
