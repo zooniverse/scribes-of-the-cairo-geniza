64f7afc1d1260c01f4537c666d30447ae92deb45
Will Granger <twgranger@gmail.com>
2018-04-03 14:42:59 -0500

    Add Translations for Bios

--- src/locales/en.js
+++ src/locales/en.js
@@ -131,5 +131,123 @@ export default {
     cancel: 'Cancel',
     done: 'Done',
     doneAndTalk: 'Done & Talk'
+  },
+  bios: {
+    Laurie: {
+      name: 'Laurie Allen',
+      description: `
+        Laurie Allen is the Assistant Director for Digital Scholarship at the
+        University of Pennsylvania Libraries.
+      `
+    },
+    Samantha: {
+      name: 'Samantha Blickhan',
+      description: `
+        Samantha Blickhan is the IMLS Postdoctoral Fellow for the Zooniverse,
+        researching best practices in crowdsourced transcription.
+      `
+    },
+    Laura: {
+      name: 'Laura Newman Eckstein',
+      description: `
+        Laura Newman Eckstein is the Judaica Digital Humanities Coordinator at the
+        University of Pennsylvania Libraries.
+      `
+    },
+    Doug: {
+      name: 'Doug Emery',
+      description: `
+        Doug Emery is a Special Collections Digital Content Programmer at the
+        Kislak Center for Special Collections, Rare Books and Manuscripts at the
+        University of Pennsylvania.
+      `
+    },
+    Mitch: {
+      name: 'Mitch Fraas',
+      description: `
+        Mitch Fraas is the Curator of Special Collections at the Kislak Center for
+        Special Collections, Rare Books and Manuscripts at the University of Pennsylvania.
+      `
+    },
+    Arthur: {
+      name: 'Arthur Kiron',
+      description: `
+        Arthur Kiron is the Schottenstein-Jesselson Curator of Judaica Collections,
+        the Head of the Library at Penn's Herbert D. Katz Center for Advanced Judaic
+        Studies, and an Adjunct Assistant Professor of History at the University
+        of Pennsylvania.
+      `
+    },
+    Moshe: {
+      name: 'Moshe Lavee',
+      description: `
+        Moshe Lavee is co-director of e-Lijah lab for crowdsourcing in Jewish Studies,
+        the BSc program in digital humanities and the Interdisciplinary Center for
+        Genizah Research at The University of Haifa. Moshe is teaching Talmud and
+        Midrash in the department of Jewish History at the University of Haifa, and
+        a specialist in Midrash of the Genizah.
+      `
+    },
+    Eve: {
+      name: 'Eve Krakowski',
+      description: `
+        Eve Krakowski is an Assistant Professor of Near Eastern Studies at Princeton
+        University, and a specialist in the documentary Geniza.
+      `
+    },
+    William: {
+      name: 'William Noel',
+      description: `
+        William Noel is the Director of The Kislak Center for Special Collections,
+        Rare Books and Manuscripts and The Schoenberg Institute for Manuscript Studies
+        at the University of Pennsylvania.
+      `
+    },
+    Vered: {
+      name: 'Vered Raziel Kretzmer',
+      description: `
+        Vered Raziel Kretzmer is the coordinator of the Genizah crowdsourcing
+        initiative at the e-Lijah lab for crowdsourcing in Jewish Studies at The
+        University of Haifa. She is a specialist in the liturgy of the Geniza.
+      `
+    },
+    Raha: {
+      name: 'Raha Rafii',
+      description: `
+        Raha Rafii is a Ph.D. candidate in the Department of Near Eastern Languages
+        and Civilizations at the University of Pennsylvania, specializing in
+        medieval Islamic history and law.
+      `
+    },
+    Becky: {
+      name: 'Becky Rother',
+      description: `
+        Becky Rother is the Zooniverse designer, working with the research team and
+        Zooniverse developers to create the best possible transcription experience.
+      `
+    },
+    Marina: {
+      name: 'Marina Rustow',
+      description: `
+        Marina Rustow is the Khedouri A. Zilkha Professor of Jewish Civilization in
+        the Near East, Professor of Near Eastern Studies and History at Princeton
+        University, and a specialist in the documentary Geniza.
+      `
+    }
+  },
+  specialThanks: {
+    main: `
+      In addition, special thanks and credit to Dr. Jean Bauer, Hal Blackburn, Jessica
+      Dummer, Timothy Dungate, Scott Enderle, Dr. Jessica Goldberg, Rebecca Hill,
+      Amey Hutchins, Dr. David Kraemer, Dr. Nita Krevans, Kate Lynch, Gayatri B. Oruganti,
+      Dr. Ben Outhwaite, Dr. Craig Perry, Besan Radwan, Jasmine Shinohara, Dr. Smadar Shtuhl,
+      Emma Stanford, and Dr. Oded Zinger for their contributions.
+    `,
+    additional: `
+      Additionally, thank you to the following institutions for providing
+      supplementary images for this project: The Berlin State Library, The
+      Bodleian Libraries at the University of Oxford, The British Library, and The
+      National Library of Israel.
+    `
   }
 };
--- src/locales/he.js
+++ src/locales/he.js
@@ -125,5 +125,104 @@ export default {
     cancel: 'בטל',
     done: 'סיים',
     doneAndTalk: 'סיים ועבור לפורום'
+  },
+  bios: {
+    Laurie: {
+      name: 'Laurie Allen',
+      description: '.לורי אלן היא מנהלת הדיגיטציה בספריות אוניברסיטת פנסילבניה'
+    },
+    Samantha: {
+      name: 'Samantha Blickhan',
+      description: '.סמנתה בליקהן היא פוסט דוקטורנטית בזוניברס, החוקרת פרקטיקות מיטביות לתעתוק המונים'
+    },
+    Laura: {
+      name: 'Laura Newman Eckstein',
+      description: '.לורה ניומן-אקשטיין היא רכזת מדעי היהדות הדיגיטליים בספריות אוניברסיטת פנסילבניה'
+    },
+    Doug: {
+      name: 'Doug Emery',
+      description: `
+        דאג אמרי הוא מתכנת תוכן דיגיטלי במרכז קיסלק לאוספים מיוחדים, ספרים נדירים וכתבי יד באוניברסיטת
+        .פנסילבניה
+      `
+    },
+    Mitch: {
+      name: 'Mitch Fraas',
+      description: `
+        מיץ' פראס הוא אוצר האוספים המיוחדים במרכז קיסלק לאוספים מיוחדים, ספרים נדירים וכתבי יד
+        .באוניברסיטת פנסילבניה
+      `
+    },
+    Arthur: {
+      name: 'Arthur Kiron',
+      description: `
+        ארתור קירון הוא אוצר אוספי היודאיקה ע"ש שוטנשטיין-יסלזון, ראש הספריה למחקרי יהדות מתקדמים
+        .במרכז הרברט ד. כץ בפנסילבניה, ומרצה להיסטוריה באוניברסיטת פנסילבניה
+      `
+    },
+    Moshe: {
+      name: 'Moshe Lavee',
+      description: `
+        משה לביא עומד בראש מעבדת אליהו לשיתוף המונים במדעי היהדות, ראש התכנית למדעי הרוח
+        הדיגיטליים וממנהלי המרכז לחקר הגניזה באוניברסיטת חיפה. מרצה לתלמוד ומדרש במחלקה לתולדות
+        .ישראל באוניברסיטת חיפה, ומתמחה בחקר המדרש בגניזה
+      `
+    },
+    Eve: {
+      name: 'Eve Krakowski',
+      description: `
+        .איב קרקובסקי היא מרצה ללימודי המזרח הקרוב באוניברסטת פרינסטון, מתמחה בגניזה הדוקומנטרית
+      `
+    },
+    William: {
+      name: 'William Noel',
+      description: `
+        ויליאם נואל הוא ראש מרכז קיסלק לאוספים מיוחדים, ספרים נדירים וכתבי יד, וראש מכון שונברג לחקר
+        .כתבי יד באוניברסיטת פנסילבניה
+      `
+    },
+    Vered: {
+      name: 'Vered Raziel Kretzmer',
+      description: `
+        ורד רזיאל קרצ'מר אחראית על פרויקט שיתוף המונים בגניזה במעבדת אליהו לשיתוף המונים במדעי
+        .היהדות באוניברסיטת חיפה, ומתמחה בחקר התפילה בגניזת קהיר
+      `
+    },
+    Raha: {
+      name: 'Raha Rafii',
+      description: `
+        Raha Rafii is a Ph.D. candidate in the Department of Near Eastern Languages
+        and Civilizations at the University of Pennsylvania, specializing in medieval Islamic history and law.
+      `
+    },
+    Becky: {
+      name: 'Becky Rother',
+      description: `
+        בקי רות'ר היא המעצבת של זוניברס, עובדת בשיתוף פעולה עם צוות המחקר והמפתחים של זוניברס
+        .ליצירת חוויית תעתוק מיטבית
+      `
+    },
+    Marina: {
+      name: 'Marina Rustow',
+      description: `
+        מרינה רוסטו עומדת בראש קתדרת כדורי א. זילכה לתרבות יהודית במזרח הקרוב, פרופסור לחקר
+        .ההיסטוריה של המזרח הקרוב באוניברסיטת פרינסטון ומתמחה בגניזה הדוקומנטרית
+      `
+    }
+  },
+  specialThanks: {
+    main: `
+      In addition, special thanks and credit to Dr. Jean Bauer, Hal Blackburn, Jessica
+      Dummer, Timothy Dungate, Scott Enderle, Dr. Jessica Goldberg, Rebecca Hill,
+      Amey Hutchins, Dr. David Kraemer, Dr. Nita Krevans, Kate Lynch, Gayatri B. Oruganti,
+      Dr. Ben Outhwaite, Dr. Craig Perry, Besan Radwan, Jasmine Shinohara, Dr. Smadar Shtuhl,
+      Emma Stanford, and Dr. Oded Zinger for their contributions.
+    `,
+    additional: `
+      Additionally, thank you to the following institutions for providing
+      supplementary images for this project: The Berlin State Library, The
+      Bodleian Libraries at the University of Oxford, The British Library, and The
+      National Library of Israel.
+    `
   }
 };
