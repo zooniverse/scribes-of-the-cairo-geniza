cdfe304da8a16633c92d04f8ec8672bec198acae
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-05-04 19:10:52 +0100

    Fix modal shadows

--- src/components/WorkflowPrompt.jsx
+++ src/components/WorkflowPrompt.jsx
@@ -4,7 +4,7 @@ import WorkflowDropdown from './WorkflowDropdown';
 const WorkflowPrompt = () => {
   return (
     <div className="workflow-prompt">
-      <div className="workflow-prompt-test">
+      <div className="workflow-prompt-content">
         <div className="workflow-prompt__header">
           <span>Choose a Workflow</span>
           <hr className="plum-line" />
