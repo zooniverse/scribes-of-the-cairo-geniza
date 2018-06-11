d61d7432f0d83ba34e027929b0862950364c10d1
Will Granger <twgranger@gmail.com>
2018-06-11 09:57:53 -0500

    Hide Workflows from WorkflowSelection Component

--- src/components/WorkflowSelection.jsx
+++ src/components/WorkflowSelection.jsx
@@ -73,18 +73,21 @@ const WorkflowSelection = ({ className, dispatch, location, history, translate, 
               href={`${classifyPath}${c.phaseOne}`}
               target="_blank"
             >
-              {translate('workflowSelection.phaseOne')} <i className="fa fa-external-link" />
+              {translate('workflowSelection.phaseOne')} <i className="fa fa-external-link-alt" />
             </a>
           </div>
           <div>
             <span className="h1-font">{translate('general.hebrew')}</span>
             <span className="primary-label">{translate('workflowSelection.phaseTwo')}</span>
-            <button
-              className="tertiary-label"
-              onClick={selectWorkflow.bind(null, c.easyHebrew)}
-            >
-              {translate('transcribeHebrew.easy')}
-            </button>
+            <div className="disabled-workflow">
+              <button
+                className="tertiary-label"
+                onClick={selectWorkflow.bind(null, c.easyHebrew)}
+              >
+                {translate('transcribeHebrew.easy')}
+              </button>
+              <span>{translate('general.comingSoon')}</span>
+            </div>
             <div className="disabled-workflow">
               <button
                 className="tertiary-label disabled-workflow"
@@ -98,24 +101,30 @@ const WorkflowSelection = ({ className, dispatch, location, history, translate, 
 
             <div>
               <span className="primary-label">{translate('workflowSelection.keywordSearch')}</span>
-              <a
-                className="tertiary-label"
-                href={`${classifyPath}${c.hebrewKeyword}`}
-                target="_blank"
-              >
-                {translate('keywordsHebrew.button')} <i className="fa fa-external-link" />
-              </a>
+              <div className="disabled-workflow">
+                <a
+                  className="tertiary-label"
+                  href={`${classifyPath}${c.hebrewKeyword}`}
+                  target="_blank"
+                >
+                  {translate('keywordsHebrew.button')} <i className="fa fa-external-link-alt" />
+                </a>
+                <span>{translate('general.comingSoon')}</span>
+              </div>
             </div>
           </div>
           <div>
             <span className="h1-font">{translate('general.arabic')}</span>
             <span className="primary-label">{translate('workflowSelection.phaseTwo')}</span>
-            <button
-              className="tertiary-label"
-              onClick={selectWorkflow.bind(null, c.easyArabic)}
-            >
-              {translate('transcribeArabic.easy')}
-            </button>
+            <div className="disabled-workflow">
+              <button
+                className="tertiary-label"
+                onClick={selectWorkflow.bind(null, c.easyArabic)}
+              >
+                {translate('transcribeArabic.easy')}
+                <span>{translate('general.comingSoon')}</span>
+              </button>
+            </div>
             <div className="disabled-workflow">
               <button
                 className="tertiary-label"
@@ -129,13 +138,16 @@ const WorkflowSelection = ({ className, dispatch, location, history, translate, 
 
             <div>
               <span className="primary-label">{translate('workflowSelection.keywordSearch')}</span>
-              <a
-                className="tertiary-label"
-                href={`${classifyPath}${c.arabicKeyword}`}
-                target="_blank"
-              >
-                {translate('keywordsArabic.button')} <i className="fa fa-external-link" />
-              </a>
+              <div className="disabled-workflow">
+                <a
+                  className="tertiary-label"
+                  href={`${classifyPath}${c.arabicKeyword}`}
+                  target="_blank"
+                >
+                  {translate('keywordsArabic.button')} <i className="fa fa-external-link-alt" />
+                </a>
+                <span>{translate('general.comingSoon')}</span>
+              </div>
             </div>
           </div>
         </div>
